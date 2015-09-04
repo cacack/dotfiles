a95e67d72a7360b441d5146df09742158ac3d898
Chris Clonch <chris@theclonchs.com>
2015-09-04 19:54:13 -0400

    Added CAS git tools.

--- linux-gnu/user/dot.profile.d/99_exec-bash.sh
+++ linux-gnu/user/dot.profile.d/99_exec-bash.sh
@@ -1,5 +1,7 @@
 # Only execute if at work.
 [ -e ~/.at_work ] || return
 
+which git-ls-projects >/dev/null 2>/dev/null || export PATH=$PATH:/home/cac21/.cas-git-tools/bin
+
 # Switch to bash if available.
 [ $(which bash) ] && exec $(which bash)
