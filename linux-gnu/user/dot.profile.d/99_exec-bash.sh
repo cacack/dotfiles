2d6c525f5d8049e6e003220213e643d14fb9a747
Chris Clonch <chris@theclonchs.com>
2015-09-15 12:33:48 -0400

    Remove CAS git tools sourcing.

--- linux-gnu/user/dot.profile.d/99_exec-bash.sh
+++ linux-gnu/user/dot.profile.d/99_exec-bash.sh
@@ -1,6 +1,4 @@
 # Only execute if at work.
-
-which git-ls-projects >/dev/null 2>/dev/null || export PATH=$PATH:/home/cac21/.cas-git-tools/bin
 [ -e ~/.cas-home ] || return
 
 # Switch to bash if available.
