3d5b9e512cbf806d5bc3c5e89effbd9d88920170
Chris Clonch <chris@theclonchs.com>
2015-09-15 12:33:18 -0400

    Change CAS detection.

--- linux-gnu/user/dot.profile.d/99_exec-bash.sh
+++ linux-gnu/user/dot.profile.d/99_exec-bash.sh
@@ -1,7 +1,7 @@
 # Only execute if at work.
-[ -e ~/.at_work ] || return
 
 which git-ls-projects >/dev/null 2>/dev/null || export PATH=$PATH:/home/cac21/.cas-git-tools/bin
+[ -e ~/.cas-home ] || return
 
 # Switch to bash if available.
 [ $(which bash) ] && exec $(which bash)
