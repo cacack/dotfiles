923de0d36e7570487717df00c9f9f725633345fc
Chris Clonch <chris@theclonchs.com>
2015-12-15 14:06:20 -0500

    Removed proxy.

--- linux-gnu/user/dot.profile.d/99_work.sh
+++ linux-gnu/user/dot.profile.d/99_work.sh
@@ -3,4 +3,4 @@
 
 which git-ls-projects >/dev/null 2>/dev/null || export PATH=$PATH:/home/cac21/.cas-git-tools/bin
 
-export http_proxy="http://cac21:junk@websensep-wcg.intra.cas.org:8080"
+#export http_proxy="http://cac21:junk@websensep-wcg.intra.cas.org:8080"
