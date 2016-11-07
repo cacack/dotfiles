40f8df9f1c19ef97b565d0ec7fb3795708b2e340
Chris Clonch <chris@theclonchs.com>
2016-11-07 07:37:19 -0500

    Adding chruby profile script

---
+++ linux-gnu/user/dot.profile.d/50_chruby.sh
@@ -0,0 +1,8 @@
+if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
+	if [ -r /usr/local/share/chruby/chruby.sh ]; then
+		source /usr/local/share/chruby/chruby.sh
+		source /usr/local/share/chruby/auto.sh
+
+		chruby ruby-2.3.1
+	fi
+fi
