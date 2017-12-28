dc95b6e6831a81bdffcb92a1eef1f0a3f98efa28
Chris Clonch <chris@theclonchs.com>
2017-12-28 15:10:45 -0500

    Fix testing scripts

--- bin/lint_ansible.sh
+++ bin/lint_ansible.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 WORKING_DIR=$(dirname "${BASH_SOURCE[0]}")
-GIT_DIR=$(git rev-parse --show-toplevel)
-RELATIVE_GIT_DIR=$(realpath --relative-to="${PWD}" "${GIT_DIR}")
+GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
 CONF_FILE="${GIT_ROOT_DIR}/.ansible-lint"
 
-find "${RELATIVE_GIT_DIR}" -type f -print0 \
+# Use git to generate a list of files (ignoring nested gits)
+git grep -I --name-only -z -e '' \
   | "${WORKING_DIR}/helpers/pipe_to_ansible_lint.sh" "${CONF_FILE}"
--- bin/lint_yaml.sh
+++ bin/lint_yaml.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 WORKING_DIR=$(dirname "${BASH_SOURCE[0]}")
-GIT_DIR=$(git rev-parse --show-toplevel)
-RELATIVE_GIT_DIR=$(realpath --relative-to="${PWD}" "${GIT_DIR}")
-CONF_FILE="${GIT_DIR}/.yamllint.yml"
+GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
+CONF_FILE="${GIT_ROOT_DIR}/.yamllint.yml"
 
-find "${RELATIVE_GIT_DIR}" -type f -print0 \
+# Use git to generate a list of files (ignoring nested gits)
+git grep -I --name-only -z -e '' \
   | "${WORKING_DIR}/helpers/pipe_to_yamllint.sh" "${CONF_FILE}"
