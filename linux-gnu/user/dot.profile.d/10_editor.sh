d5ed2f7c0c7817d978d505492b990d7b9dbd51c5
Chris Clonch <chris@theclonchs.com>
2017-12-28 13:33:12 -0500

    Fix check, lint, and test failures

--- linux-gnu/user/dot.profile.d/10_editor.sh
+++ linux-gnu/user/dot.profile.d/10_editor.sh
@@ -1,7 +1,9 @@
-# Set the default editor to vim.                                                                        
-if [ -x `which vim` ]; then                                                                             
-  export EDITOR=vim                                                                                     
-elif [ -x `which vi` ]; then                                                                            
-  export EDITOR=vi                                                                                      
-fi                                                                                                      
+#!/usr/bin/env bash
+
+# Set the default editor to vim.
+if [ -x "$(which vim)" ]; then
+  export EDITOR=vim
+elif [ -x "$(which vi)" ]; then
+  export EDITOR=vi
+fi
 export VISUAL=$EDITOR
--- linux-gnu/user/dot.profile.d/20_aliases.sh
+++ linux-gnu/user/dot.profile.d/20_aliases.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 # Make some possibly destructive commands more interactive.
 alias rm='rm -i'
 alias mv='mv -i'
 alias cp='cp -i'
- 
+
 # Add some easy shortcuts for formatted directory listings and add a touch of color.
 alias ls='ls -F --color=auto'
 alias ll='ls -l'
@@ -11,7 +13,7 @@ alias lla='la -l'
 alias lx='ls -X'
 alias llx='lx -l'
 alias l='ls -1'
- 
+
 # Make grep more user friendly by highlighting matches
 # and exclude grepping through .svn folders.
 alias grep='grep --color=auto --exclude-dir=\.svn'
