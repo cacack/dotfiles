b61370123be2e2f064ec43feaa98b78ef290321e
Chris Clonch <chris@theclonchs.com>
2018-02-02 09:38:40 -0500

    Cleanup chruby shell profile hook

--- linux-gnu/user/dot.profile.d/50_chruby.sh
+++ linux-gnu/user/dot.profile.d/50_chruby.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC1091
-
 # Ansible managed
 
 if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
   if [ -r /usr/local/share/chruby/chruby.sh ]; then
     source /usr/local/share/chruby/chruby.sh
     source /usr/local/share/chruby/auto.sh
-
-    chruby ruby-2.3.3
   fi
 fi
