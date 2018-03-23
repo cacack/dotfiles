bebc51e56456aaaf2b0d1b0415dfbe8c1615ff46
Chris Clonch <chris@theclonchs.com>
2018-03-22 22:20:39 -0400

    Disable shellcheck on sourcing chruby

--- linux-gnu/user/dot.profile.d/50_chruby.sh
+++ linux-gnu/user/dot.profile.d/50_chruby.sh
@@ -3,7 +3,9 @@
 
 if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
   if [ -r /usr/local/share/chruby/chruby.sh ]; then
+    # shellcheck disable=SC1091
     source /usr/local/share/chruby/chruby.sh
+    # shellcheck disable=SC1091
     source /usr/local/share/chruby/auto.sh
   fi
 fi
