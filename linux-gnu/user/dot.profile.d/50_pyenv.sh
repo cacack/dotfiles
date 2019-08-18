dc0acf30ef38326773140358bf2a822948bb7c0c
Chris Clonch <chris@theclonchs.com>
2019-08-17 22:34:56 -0400

    Update pyenv shell init

--- linux-gnu/user/dot.profile.d/50_pyenv.sh
+++ linux-gnu/user/dot.profile.d/50_pyenv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-  if [ -r ~/devel/others/pyenv ]; then
-    export PYENV_ROOT="${HOME}/devel/others/pyenv"
-    export PATH="${HOME}/devel/others/pyenv/bin:$PATH"
+  if [ -r ~/.pyenv ]; then
+    export PYENV_ROOT="${HOME}/.pyenv"
+    export PATH="${HOME}/.pyenv/bin:$PATH"
     if command -v pyenv 1>/dev/null 2>&1; then
       eval "$(pyenv init -)"
     fi
