2ffe5fa1b10c0fc01173901d3bcc3dc9bc69d127
Chris Clonch <chris@theclonchs.com>
2019-08-19 16:39:48 +0000

    Remove shellcheck from chruby init

--- linux-gnu/user/dot.profile.d/50_chruby.sh
+++ linux-gnu/user/dot.profile.d/50_chruby.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/env bash
 # Ansible managed
 
 if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
   if [ -r /usr/local/share/chruby/chruby.sh ]; then
-    # shellcheck disable=SC1091
     source /usr/local/share/chruby/chruby.sh
-    # shellcheck disable=SC1091
     source /usr/local/share/chruby/auto.sh
   fi
 fi
